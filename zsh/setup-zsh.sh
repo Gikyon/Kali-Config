80cdd2a4f91c8b854a156085073d33df28c37ae8
Micah Joshua Rahardjo <158001082+Gikyon@users.noreply.github.com>
2026-01-26 15:47:10 -0500

    update zsh

--- zsh/setup-zsh.sh
+++ zsh/setup-zsh.sh
@@ -16,61 +16,54 @@ setup_user() {
   USER_CUSTOM="$USER_ZSH/custom"
 
   # -------------------------
-  # Install Oh My Zsh
+  # Install Oh My Zsh (verify real install)
   # -------------------------
-  if [ ! -d "$USER_ZSH" ]; then
+  if [ ! -f "$USER_ZSH/oh-my-zsh.sh" ]; then
     echo "  └─ Installing Oh My Zsh"
+
+    # Clean broken install if present
+    rm -rf "$USER_ZSH"
+
     if [ "$USERNAME" = "root" ]; then
       RUNZSH=no CHSH=no sh -c \
-        "curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+        "curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh"
     else
       sudo -u "$USERNAME" env RUNZSH=no CHSH=no sh -c \
-        "curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+        "curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh"
     fi
   else
     echo "  └─ Oh My Zsh already installed"
   fi
 
   # -------------------------
-  # Plugins
+  # Plugins (install as user)
   # -------------------------
-  mkdir -p "$USER_CUSTOM/plugins"
+  if [ "$USERNAME" = "root" ]; then
+    mkdir -p "$USER_CUSTOM/plugins"
 
-  [ -d "$USER_CUSTOM/plugins/zsh-autosuggestions" ] || \
-    git clone https://github.com/zsh-users/zsh-autosuggestions \
-      "$USER_CUSTOM/plugins/zsh-autosuggestions"
+    [ -d "$USER_CUSTOM/plugins/zsh-autosuggestions" ] || \
+      git clone https://github.com/zsh-users/zsh-autosuggestions \
+        "$USER_CUSTOM/plugins/zsh-autosuggestions"
 
-  [ -d "$USER_CUSTOM/plugins/zsh-syntax-highlighting" ] || \
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting \
-      "$USER_CUSTOM/plugins/zsh-syntax-highlighting"
+    [ -d "$USER_CUSTOM/plugins/zsh-syntax-highlighting" ] || \
+      git clone https://github.com/zsh-users/zsh-syntax-highlighting \
+        "$USER_CUSTOM/plugins/zsh-syntax-highlighting"
+  else
+    sudo -u "$USERNAME" mkdir -p "$USER_CUSTOM/plugins"
+
+    sudo -u "$USERNAME" sh -c '
+      [ -d "'"$USER_CUSTOM/plugins/zsh-autosuggestions"'" ] || \
+        git clone https://github.com/zsh-users/zsh-autosuggestions \
+          "'"$USER_CUSTOM/plugins/zsh-autosuggestions"'"
+    '
+
+    sudo -u "$USERNAME" sh -c '
+      [ -d "'"$USER_CUSTOM/plugins/zsh-syntax-highlighting"'" ] || \
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting \
+          "'"$USER_CUSTOM/plugins/zsh-syntax-highlighting"'"
+    '
+  fi
 
   # -------------------------
   # zshrc
-  # -------------------------
-  cp "$ZSHRC_SOURCE" "$USER_HOME/.zshrc"
-
-  # -------------------------
-  # Ownership
-  # -------------------------
-  chown -R "$USERNAME:$USERNAME" "$USER_HOME/.oh-my-zsh" "$USER_HOME/.zshrc"
-
-  echo "  └─ Done"
-}
-
-# -------------------------
-# Normal users
-# -------------------------
-for USER_HOME in /home/*; do
-  [ -d "$USER_HOME" ] || continue
-  USERNAME="$(basename "$USER_HOME")"
-  id "$USERNAME" >/dev/null 2>&1 || continue
-  setup_user "$USERNAME" "$USER_HOME"
-done
-
-# -------------------------
-# Root
-# -------------------------
-setup_user "root" "/root"
-
-echo "[✔] Zsh configured for all users (including root)"
-echo "Log out and back in, or run: zsh"
+  # ---
